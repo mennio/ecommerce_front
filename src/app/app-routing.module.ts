4c9191c00dd4e812702fdb94e80807317037d48f
SharmAurel <sharmaa.aurelien@gmail.com>
2022-05-01 17:11:35 +0200

    archi0105

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,13 @@ import { HomeComponent } from './home/home.component';
 import { PCAccessoiresComponent } from './pc-accessoires/pc-accessoires.component';
 import { PCBUREAUComponent } from './pc-bureau/pc-bureau.component';
 import { PCPORTABLEComponent } from './pc-portable/pc-portable.component';
+import { TelSmartphoneComponent } from './tel-smartphone/tel-smartphone.component';
+import { TelFixeComponent } from './tel-fixe/tel-fixe.component';
+import { TelAccessoiresComponent } from './tel-accessoires/tel-accessoires.component';
+import { StockageDisquedurComponent } from './stockage-disquedur/stockage-disquedur.component';
+import { StockageCleusbComponent } from './stockage-cleusb/stockage-cleusb.component';
+import { StockageAccessoiresComponent } from './stockage-accessoires/stockage-accessoires.component';
+import { PanierComponent } from './panier/panier.component';
 
 const routes: Routes = [
   {path : "", component: HomeComponent},
@@ -13,6 +20,17 @@ const routes: Routes = [
   {path : "PcBureau", component: PCBUREAUComponent},
   {path : "PcAccessoires", component: PCAccessoiresComponent},
   {path : "Connexion", component: ConnexionComponent},
+  {path : "TelSmartphone", component: TelSmartphoneComponent},
+  {path : "TelFixe", component: TelFixeComponent},
+  {path : "TelAccessoires", component: TelAccessoiresComponent},
+  {path : "StockageDisquedur", component: StockageDisquedurComponent},
+  {path : "StockageCleusb", component: StockageCleusbComponent},
+  {path : "StockageAccessoires", component: StockageAccessoiresComponent},
+  {path : "Panier", component: PanierComponent},
+
+
+
+
   /*{path : '',
    component : AppComponent,
    children : [
